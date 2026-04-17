c64810cad2bc53c3c7252d2a9c7f0a47595d80b1
Viktor <viktor4strlsov@gmail.com>
2026-04-17 16:31:55 +0600

    main Poto my

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import cv2
 
-cap = cv2.VideoCapture('video.mp4')
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*"MJPG")
 
-print(cap.isOpened())
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+out = cv2.VideoWriter("output.jpg", fourcc, 25.0, (frame_width, frame_height))
 
-while(cap.isOpened()):
+while cap.isOpened():
     ret, frame = cap.read()
+    print('running')
 
     if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("frame", gray)
-        # cv2.imshow("frame", frame)
+        frame = cv2.flip(frame, 1)
+        out.write(frame)
+        cv2.imshow('frame', frame)
 
-        if cv2 .waitKey(25) & 0xFF == ord('q'):
-            break
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
     else:
-        print("error")
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
