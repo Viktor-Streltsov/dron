ec9904f5d21d8e9bb852b7bd0a00e48431959c73
Viktor <viktor4strlsov@gmail.com>
2026-04-17 16:15:30 +0600

    mian Video

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import cv2
 
-img_file = "./berds.jpg"
-img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+cap = cv2.VideoCapture('video.mp4')
 
+print(cap.isOpened())
 
-title = "IMG"
-x, y = 100, 100
+while(cap.isOpened()):
+    ret, frame = cap.read()
 
-while True:
-    cv2.imshow(title, img)
-    cv2.moveWindow(title, x, y)
-    key = cv2.waitKey(0) & 0xFF
-    print(key, chr(key))
+    if ret:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("frame", gray)
+        # cv2.imshow("frame", frame)
 
-    if key == ord("a"):
-        x -= 10
-    elif key == ord("s"):
-        y += 10
-    elif key == ord("w"):
-        y -= 10
-    elif key == ord("d"):
-        x += 10
-    elif key == ord("q") or key == 27:
+        if cv2 .waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        print("error")
         break
 
+cap.release()
 cv2.destroyAllWindows()
